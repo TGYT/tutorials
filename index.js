0d97eb59447835ad21d842cb8acc31a6bf3c45b7
Glitch (youtube-tutorial) <none>
2018-04-17 11:28:30 +0000

    🎣🌒 Checkpoint ./index.js:707144/8

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'youtube') {
-        message.channel.send('Hi YouTube! Subscribe to **Bolt Codes** for more videos, tutorials, series, etc.');
+        message.channel.send('Hi, **YouTube**! Subscribe to **Bolt Codes** for more videos, tutorials, series, etc.');
     }
 });
 
