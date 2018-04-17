9854f905fb72f2ace39725500bb3ae8e4b2a4f2e
Glitch (youtube-tutorial) <none>
2018-04-17 12:01:30 +0000

    🐲💵 Checkpoint ./index.js:707144/1846

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js'); // Discrd.JS package
 const client = new Discord.Client(); // New Discord Client
-const prefix = '!'; // you can change the string to whatever you want
 
 client.on('ready', () => { // Ready Event
     console.log('Bot has started!');
@@ -8,13 +7,9 @@ client.on('ready', () => { // Ready Event
 
 client.on('message', message => { // Message Event
   
-  let msg = message.content.toLowerCase();
-  
-    if (msg === prefix + 'youtube') {
+    if (message.content === 'youtube') {
         message.channel.send('Hi, **YouTube**! Subscribe to **Bolt Codes** for more videos, tutorials, series, etc.');
     }
 });
 
 client.login(process.env.SECRET); // My token is hidden so I don't have to regenerate it in every new video
-
-// so let's test it out just type node index.js in console to run it
