d0a3a0668ee1e4f9cc882d802ba39f0ca4d0af1d
Glitch (youtube-tutorial) <none>
2018-04-18 12:21:49 +0000

    🔑🎤 Checkpoint ./index.js:707144/1018

--- index.js
+++ index.js
@@ -8,11 +8,21 @@ client.on('ready', () => { // Ready Event
 
 client.on('message', message => { // Message Event
   
-  let msg = message.content.toLowerCase();
+  if (message.author.bot) return undefined; // bot doesn't reply to itself
   
-    if (msg === prefix + 'youtube') {
+  let msg = message.content.toLowerCase(); // message's content to lowercase letter
+  let args = message.content.slice(prefix.length).trim().split(' '); // arguments 
+  let command = args.shift().toLowerCase(); // shift arguments to lower case
+  
+    if (command === 'youtube') {
         message.channel.send('Hi, **YouTube**! Subscribe to **Bolt Codes** for more videos, tutorials, series, etc.');
     }
+  if (command === 'say') {
+    
+    let say = args.join(' ');
+    message.delete(); // deletes the content (!say <text>)
+    message.channel.send(say);
+  }
 });
 
 client.login(process.env.SECRET); // My token is hidden so I don't have to regenerate it in every new video
