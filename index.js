b230e810b8a63297451be2812a56dcb53da0f6a3
Glitch (youtube-tutorial) <none>
2018-04-20 15:50:14 +0000

    📺⛑ Checkpoint ./index.js:707144/135

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ client.on('ready', () => { // Ready Event
 
 client.on('message', message => { // Message Event
   
-  if (message.author.bot) return undefined; // bot doesn't reply to itself
+  if (message.author.bot) return undefined; // Bot doesn't reply to itself
   
-  let msg = message.content.toLowerCase(); // message's content to lowercase letter
-  let args = message.content.slice(prefix.length).trim().split(' '); // arguments 
-  let command = args.shift().toLowerCase(); // shift arguments to lower case
+  let msg = message.content.toLowerCase(); // Message's content to lowercase letter
+  let args = message.content.slice(prefix.length).trim().split(' '); // Arguments 
+  let command = args.shift().toLowerCase(); // Shift arguments to lower case
   
   if (command === 'youtube') {
     message.channel.send('Hi, **YouTube**! Subscribe to **Bolt Codes** for more videos, tutorials, series, etc.');
@@ -20,20 +20,20 @@ client.on('message', message => { // Message Event
   
   if (command === 'say') {
     let say = args.join(' ');
-    message.delete(); // deletes the content (Usage: !say <text>)
+    message.delete(); // Deletes the content (Usage: !say <text>)
     message.channel.send(say);
   }
   if (command === 'avatar') {
-    let user = message.mentions.users.first() || message.author; // mention to get avatar or if no mention it will take author's avatar and send it!
+    let user = message.mentions.users.first() || message.author; // Mention to get avatar or if no mention it will take author's avatar and send it!
     
     // avatar embed
     let embed = new Discord.RichEmbed()
-    .setAuthor(`${user.username}`)
-    .setImage(user.displayAvatarURL)
-    .setColor('RANDOM') // it will generate random colors now let's test it out!
-    // finally send the avatar
+    .setAuthor(`${user.username}'s Avatar`)
+    .setImage(user.displayAvatarURL) // User's Avatar
+    .setColor('RANDOM') // It will generate random colors now let's test it out!
+    // Sends the avatar embed in the channel.
     message.channel.send(embed)
   }
 });
 
-client.login(process.env.SECRET); // My token is hidden so I don't have to regenerate it in every new video
+client.login(process.env.SECRET); // My token is hidden
