2616bd500246179ded8a8034b495df44c52678a9
Glitch (youtube-tutorial) <none>
2018-04-26 14:58:41 +0000

    💏✊ Checkpoint ./commands/ban.js:707144/868

---
+++ commands/ban.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args) => { // Exporting events, and etc.
+  
+  if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send('You don\'t have permission to ban members');
+  
+  let member = message.mentions.members.first();
+  if (!member) return message.channel.send('Please mention a member to ban!');
+  if (!member.bannable) return message.channel.send('You cannot ban a member with role higher or equal than you');
+  
+  let reason = args.slice(1).join(' ');
+  
+  await member.ban(reason)
+  .catch(e => console.log(e));
+  
+  let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.tag, message.author.displayAvatarURL)
+  .setTitle('Banned!')
+  .setDescription(
+}
