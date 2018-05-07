52e2dd6fcbe81a701f6fd01c50e33dc0f6518ec7
Glitch (youtube-tutorial) <none>
2018-05-07 14:55:20 +0000

    👓🚩 Checkpoint ./commands/userinfo.js:707144/1190

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,16 +1,17 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js'); // discord.js package
 
-exports.run = (client, message, args) => {
+exports.run = (client, message, args) => { // exporting message event, client object, etc.
   
-  let user = message.mentions.users.first();
-  let embed = new Discord.RichEmbed() // RichEmbed()
-  .setAuthor(user.tag, user.displayAvatarURL)
-  .setThumbnail(user.displayAvatarURL())
-  .addField('ID', user.id, true)
-  .addField('Username', user.tag, true)
+  let user = message.mentions.users.first(); // User Mention
+  let embed = new Discord.RichEmbed() // Embed Constructor
+  .setAuthor(`${user.tag}'s Info`, user.displayAvatarURL)
+  .setThumbnail(user.displayAvatarURL) // User's Avatar
+  .setColor('RANDOM') // Generates random color
+  .addField('ID', user.id, true) // User ID
+  .addField('Username', user.username, true) // Username
+  .addField('Discrim', user.discriminator, true) // User Tag
+  .addField('Status', user.presence.status, true) // User Status (online, idle, do not disturb, invisible/offline)
   .addField('Bot?', user.bot, true)
-  .addField('Status', user.presence.status, true)
-  .addField('Game', user.presence.activity.name, true)
-  .setTimestamp()
-  message.channel.send(embed);
+  .setThumbnail()
+  message.channel.send(embed) // Sends the embed
 };
