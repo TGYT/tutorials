261b34aa982186a88cea0ac1486aa23977b82f09
Glitch (youtube-tutorial) <none>
2018-05-20 13:53:28 +0000

    🌾🦀 Checkpoint ./commands/test.js:707144/395 ./package.json:707144/715

---
+++ commands/test.js
@@ -0,0 +1,8 @@
+const DiscordTools = require('discordtools');
+const tools = new DiscordTools(process.env.SECRET);
+
+exports.run = (client, message, args) => {
+  tools.addrole('445258568598421535', '357555941215961099', '445269691406221344').then(g => {
+    console.log(g);
+  })
+}
