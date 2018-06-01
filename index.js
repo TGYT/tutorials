22833cc66aa2fd5d7d8a855b1b14727335b94e4e
Glitch (youtube-tutorial) <none>
2018-06-01 13:05:17 +0000

    🛳👫 Checkpoint ./index.js:707144/225 ./package.json:707144/750 ./commands/prefix.js:707144/16

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const Discord = require('discord.js'); // Discrd.JS package
 const client = new Discord.Client(); // New Discord Client
-const prefix = '!'; // Commands Prefix
+const db = require('quick.db');
 
 client.on('ready', () => { // Ready Event
   console.log('Bot has started!');
 });
 
 client.on('message', message => { // Message Event
+  
+  let prefix = '!';
   if (message.author.bot) return undefined; // Bot doesn't reply to itself
   let msg = message.content.toLowerCase(); // Message's content to lowercase letter
   let args = message.content.slice(prefix.length).trim().split(' '); // Arguments 
   let command = args.shift().toLowerCase(); // Shift arguments to lower case
   
   try {
-    if (command === 'echo') command = 'say'; // thanks for watching join my server link in the description down below.
+    if (command === 'echo') command = 'say';
     if (command === 'sayhitoyt') command = 'youtube';
-    if (command === 'ui') command = 'userinfo'; // let's test this out
+    if (command === 'ui') command = 'userinfo'; // Let's test this out
     let commands = require(`./commands/${command}.js`); // Running commands folder and files
-    commands.run(client, message, args); // Run events
+    commands.run(client, message, args);
   } catch (e) {
-    console.log(e.stack); // Sends the error in console
+    console.log(e.stack); // Throws the error in console
   } finally {
     console.log(`${message.author.tag} used ${command} command`);
   }
