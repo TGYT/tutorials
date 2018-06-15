63a6c5f0771ff03ae5261ecf032172547a4d3bda
Glitch (youtube-tutorial) <none>
2018-06-15 11:03:09 +0000

    💎👌 Checkpoint ./commands/userinfo.js:707144/71 ./index.js:707144/262

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -2,16 +2,16 @@ const Discord = require('discord.js'); // discord.js package
 
 exports.run = (client, message, args) => { // exporting message event, client object, etc.
   
-  let user = message.mentions.users.first() || message.author; // User Mention
+  let user = message.mentions.users.first() || message.author; // Get User from mention
   let embed = new Discord.RichEmbed() // Embed Constructor
   .setAuthor(`${user.tag}'s Info`, user.displayAvatarURL)
   .setThumbnail(user.displayAvatarURL) // User's Avatar
   .setColor('RANDOM') // Generates random color
   .addField('ID', user.id, true) // User ID
   .addField('Username', user.username, true) // Username
-  .addField('Discrim', user.discriminator, true) // User Tag
+  .addField('Discrim', user.discriminator, true) // User Tag/discriminator
   .addField('Status', user.presence.status, true) // User Status (online, idle, do not disturb, invisible/offline)
-  .addField('Bot?', user.bot, true)
+  .addField('Bot?', user.bot, true) // if the user is bot or not
   .setThumbnail()
-  message.channel.send(embed) // Sends the embed
+  message.channel.send(embed) // Sends the embed in the channel
 };
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const prefix = '!';
 
 client.on('ready', () => { // Ready Event
   console.log('Bot has started!');
+  client.user.setStatus('dnd'); // You can set it to either online, idle, offline or dnd | client status
+  client.user.setActivity(`with YouTube with ${client.users.size}`); // set game status
+  
 });
 
 client.on('message', async message => { // Message Event
