5b2598b328f277b02e2386bcc3e81f34633a2e5f
boltxyz <bolt8bp@gmail.com>
2018-12-02 15:03:26 +0400

    Done

---
+++ electron-tutorial/index.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+let mainWindow;
+
+function createWindow () {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: 'YouTube Application'
+    });
+
+    mainWindow.loadFile('index.html');
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('activate', function () {
+    if (mainWindow === null) createWindow();
+});
